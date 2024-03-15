4a41276067f063a9716250c8857c8519c4c6f9b3
Dakotaflores <143897722+Dakotaflores@users.noreply.github.com>
2024-03-14 23:05:05 -0400

    prueba1.7

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -9,9 +9,9 @@ void exercise_1(string s1, string s2, string s3) {
   string s2 = "fox";
   string s3 = "firewood";
 
-  if (s1 > s2 && s1 > s3) {
+  if (s1 < s2 && s1 < s3) {
     cout << s1 ;
-  } else if(s2 > s1 && s2 > s3) {
+  } else if(s2 < s1 && s2 < s3) {
     cout << s2;
   } else {
     cout << s3;
