8afc6d9531fba6299e22bcc58433a6e192126e1e
Dakotaflores <143897722+Dakotaflores@users.noreply.github.com>
2024-03-14 23:34:57 -0400

    prueba1/10

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -5,9 +5,10 @@
 using namespace std;
 
 void exercise_1(string s1, string s2, string s3) { 
+  
   string s1 = "fire";
   string s2 = "fox";
-  string s3 = "firewood";
+  string s3 = "firewood"; 
 
   if (s1 < s2 && s1 < s3) {
     cout << s1 ;
