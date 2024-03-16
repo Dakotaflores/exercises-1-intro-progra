5e731b10860f806736343354a2ea2ba92eb5cceb
Dakotaflores <143897722+Dakotaflores@users.noreply.github.com>
2024-03-16 09:39:27 -0400

    minu789

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -157,7 +157,15 @@ void exercise_8(long int seconds) {
   long int minutes;
   long int new_seconds;
 
-  if (seconds > 0) {
+  if (seconds > 0 && hour >= 10) {
+    hour = seconds / 3600;
+    new_seconds = seconds % 3600;
+    minutes = new_seconds / 60;
+    seconds = new_seconds % 60;
+
+    cout << hour << ":" << minutes << ":" << seconds << endl;
+
+  } else if (seconds > 0 && hour < 10) {
     hour = seconds / 3600;
     new_seconds = seconds % 3600;
     minutes = new_seconds / 60;
