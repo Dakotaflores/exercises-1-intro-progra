bccc5c6dc90cb6bf16dc6c9940ae064aa7e5ab62
Dakotaflores <143897722+Dakotaflores@users.noreply.github.com>
2024-03-16 14:28:43 -0400

    inhuy567

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -252,7 +252,7 @@ void exercise_12(string color1, int numb1,
 string exercise_13(int age, int years_of_experience) {
   // TODO: YOUR CODE HERE
 
-  if (age < 18) {
+  /*if (age < 18) {
     cout << "El candidato no cumple con la edad mínima requerida." << endl;
 
   } else if (years_of_experience >= 5) {
@@ -267,21 +267,22 @@ string exercise_13(int age, int years_of_experience) {
 
 // Imprimir el título del puesto para el que es elegible el candidato
   cout << "El título del puesto para el que es elegible el candidato es: " << title << endl;
-  
-  return "";
-
+*/
 }
 
 string exercise_14(int number_of_docs) {
   // TODO: YOUR CODE HERE
 
-  if (num_documents == 0) {
+  if (number_of_docs == 0) {
     cout << "No se encontraron documentos" << endl;
-  } else if (num_documents == 1) {
+
+  } else if (number_of_docs == 1) {
     cout << "Se encontro un documento" << endl;
+    
   } else {
-    cout << num_documents << " documentos encontrados" << endl;
+    cout << number_of_docs << " documentos encontrados" << endl;
   }
+
 }
 
 void exercise_15(int a, int b, int c) {
