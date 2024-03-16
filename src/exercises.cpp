8379dcddce8cddc6327143caa22422df8f31d0a5
Dakotaflores <143897722+Dakotaflores@users.noreply.github.com>
2024-03-15 22:45:36 -0400

    prueba7.77

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -99,9 +99,6 @@ void exercise_6(int number) {
 void exercise_7(double r) {
   // TODO: YOUR CODE HERE
 
-const double pi = 3.14;
-const int directions = 4;  
-
     if(r > 0 ) {
         double result = directions * pi * radio * r;
     
