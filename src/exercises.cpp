8f5177aebd80ad4cb5dfda08093f7f2dc4270bdb
Dakotaflores <143897722+Dakotaflores@users.noreply.github.com>
2024-04-28 00:53:51 -0400

    enhijuyt67

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -192,7 +192,7 @@ string exercise_9(string s1, string s2, string s3, string s4, string s5) {
     cout << "Hemos encontrado algo!" << endl;
   }
   else {
-    cout << s1[0] <<  s2[0] << s3[0] << s4[0] << s5[0] << endl;
+    //cout << s1[0] <<  s2[0] << s3[0] << s4[0] << s5[0] << endl;
     //cout << "Aun sin suerte" << endl;
   }
 
