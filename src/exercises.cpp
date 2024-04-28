94fcd252df1ab37557130220125aedf4f876d558
Dakotaflores <143897722+Dakotaflores@users.noreply.github.com>
2024-04-27 23:49:49 -0400

    exe9,09

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -185,14 +185,14 @@ void exercise_8(long int seconds) {
 
 string exercise_9(string s1, string s2, string s3, string s4, string s5) {
   // TODO: YOUR CODE HERE
-
- 
-    // Verificar si el primer caracter de la primera palabra y el primer caracter de la última palabra son iguales
-    /*if (string s1[0] == string s5[0]) {
-     cout << string s1[0] << sting s2[0] << string s3[0] << string s4[0] << string s5[0] << endl << "Hemos encontrado algo!" << endl;
-    } else {
-      cout << string s1[0] << sting s2[0] << string s3[0] << string s4[0] << string s5[0] << endl << "Aun sin suerte" << endl;
-    }*/
+  //getline(cin, s1, s2, s3, s4, s5);
+  
+  if(s1[0] == s5[0]) {
+    cout << "Hemos encontrado algo!";
+  }
+  else(s1[0] != s5[0]) {
+    cout << "Aun sin suerte";
+  }
 
 }
 
