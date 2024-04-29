fa03f10e30b3d5b2c98a5a11d1ebc46a9697affa
Dakotaflores <143897722+Dakotaflores@users.noreply.github.com>
2024-04-28 20:03:54 -0400

    ahorasi876

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -192,9 +192,10 @@ string exercise_9(string s1, string s2, string s3, string s4, string s5) {
   //string First_char_last_word = s5[0]
 
   if( s1[0] == s5[0] ) {
+    //cout << s1[0] <<  s2[0] << s3[0] << s4[0] << s5[0] << endl;
     cout << "Hemos encontrado algo!" << endl;
   }
-  else {
+  else if ( s1[0] != s5[0]){
     cout << "Aun sin suerte" << endl;
   }
 
