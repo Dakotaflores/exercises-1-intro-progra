3ea04a517d5b8f37a22f398625df0ad5ad7d172e
Dakotaflores <143897722+Dakotaflores@users.noreply.github.com>
2024-04-29 10:12:35 -0400

    ultimofinexi

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -168,7 +168,7 @@ void exercise_8(long int seconds) {
       cout << ((hour < 10) ? "0" : "") << hour << ":" << ((minutes < 10 ) ? "0" : "") << minutes << ":" << ((new_seconds < 10) ? "0" : "") << new_seconds << endl;
     }
     else {
-      cout << "Error: Inputs seconds cannot be negative." << endl;
+      cout << "Error: Input seconds cannot be negative." << endl;
     }
     
 
@@ -186,11 +186,11 @@ string exercise_9(string s1, string s2, string s3, string s4, string s5) {
 
   if(s1[0] == s5[0]) {
     cout << s1[0] <<  s2[0] << s3[0] << s4[0] << s5[0] << endl;
-    cout << "Hemos encontrado algo!" << endl;
+    return "Hemos encontrado algo!";
   }
-  else {
+   else {
     cout << s1[0] << s2[0] << s3[0] << s4[0] << s5[0] << endl;
-    cout << "Aun sin suerte" << endl;
+    return "Aun sin suerte";
   }
   return"";
 
