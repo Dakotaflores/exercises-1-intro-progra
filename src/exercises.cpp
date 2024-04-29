b15ef8a2eb262a22bf104a3890b9e9c87ec36c12
Dakotaflores <143897722+Dakotaflores@users.noreply.github.com>
2024-04-28 20:23:15 -0400

    condirme450

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -186,17 +186,15 @@ void exercise_8(long int seconds) {
 string exercise_9(string s1, string s2, string s3, string s4, string s5) {
   // TODO: YOUR CODE HERE
 
-  cout << "s1[0] s2[0] s3[0] s4[0] s5[0]" << endl;
-
   //string First_char_first_word = s1[0];
   //string First_char_last_word = s5[0]
 
   if( s1[0] == s5[0] ) {
-    //cout << s1[0] <<  s2[0] << s3[0] << s4[0] << s5[0] << endl;
+    cout << s1[0] <<  s2[0] << s3[0] << s4[0] << s5[0] << endl;
     cout << "Hemos encontrado algo!" << endl;
   }
   else if ( s1[0] != s5[0]){
-    //cout << s1[0] << s2[0] << s3[0] << s4[0] << s5[0] << endl;
+    cout << s1[0] << s2[0] << s3[0] << s4[0] << s5[0] << endl;
     cout << "Aun sin suerte" << endl;
   }
 
