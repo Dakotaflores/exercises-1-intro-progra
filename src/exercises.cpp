d92bfc12a0f0f1bf830f243348ea1829dff2c581
Dakotaflores <143897722+Dakotaflores@users.noreply.github.com>
2024-05-01 02:16:45 -0400

    confirmexe16,.1

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -302,7 +302,7 @@ void exercise_15(int a, int b, int c) {
 
 void exercise_16(int debut, int fin) {
   // TODO: YOUR CODE HERE
-  
+
 if((debut < 0 || debut > 24 || fin < 0 || fin > 24)) {
     cout << "Las horas deben estar entre 0 y 24!" << endl;
     return;
@@ -335,5 +335,5 @@ if((debut < 0 || debut > 24 || fin < 0 || fin > 24)) {
   if (horas_alto > 0) {
     cout << horas_alto << " horas(s) con el tarifario de " << costo_alto << " bolivianos(s)" << endl;
   }
-cout << "El monto total a pagar es de " << total << "bolivianos(s)." << endl;
+cout << "El monto total a pagar es de " << total << " bolivianos(s)." << endl;
 }
